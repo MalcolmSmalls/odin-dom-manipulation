d00cde3242695bc0de05a8c4ce3ec5f9f715b7fc
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-01 12:50:48 -0700

    Add DIV with an h1 inside

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ h3.style.color = "blue";
 h3.textContent = "I'm a blue h3!";
 
 body.insertBefore(h3, redParagraph);
+
+const div = document.createElement('div');
+
+div.style.cssText = 'border-style:solid; border-color: black; background-color: pink;'
+
+const h1 = document.createElement('h1')
+h1.textContent = "I'm in a div!"
+
+body.appendChild(div)
+
+div.appendChild(h1)

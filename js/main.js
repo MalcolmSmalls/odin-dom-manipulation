61a91a9a15ad227b12c7e6aa890925a76a6b3ece
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-01 12:42:38 -0700

    Finish adding h3 through dom maniuplation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-const redParagraph = document.createElement('p')
+const redParagraph = document.createElement('p');
 
-redParagraph.style.color = "red"
+redParagraph.style.color = "red";
 
-redParagraph.textContent = 'Hey I\'m red'
+redParagraph.textContent = 'Hey I\'m red';
 
-body.appendChild(redParagraph)
+body.appendChild(redParagraph);
 
+const h3 = document.createElement('h3');
+
+h3.style.color = "blue";
+
+h3.textContent = "I'm a blue h3!";
+
+body.insertBefore(h3, redParagraph);
